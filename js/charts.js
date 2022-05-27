f8f4e0fcabce9c91e3c797efd6b04f77c0a080cc
Meleake Wubbie <meleake1@uw.edu>
2022-05-26 23:23:02 -0700

    rent vs pop chart added

--- js/charts.js
+++ js/charts.js
@@ -1,32 +1,36 @@
 let chartdata = [];
-fetch("https://services.arcgis.com/ZOyb2t4B0UYuYNYH/arcgis/rest/services/Typology/FeatureServer/0/query?where=1%3D1&outFields=YEAR,PRICE_NOM&returnGeometry=false&outSR=4326&f=json")
-    .then(response => {
-        return response.json()
-    })
-    .then(data => {
-        console.log(data.features[100])
-        console.log(data.features[255].attributes.YEAR)
-        console.log(data.features[255].attributes.PRICE_NOM)
-        chartdata = [];
-        //chartdata[0] = "Single Family Home Sales Prices"
-        for(let i = 0; i < data.features.length; i++)
-        {
-            let d = data.features[i].attributes;
-            // console.log(d);
-            if(d.YEAR == null || d.PRICE_NOM == null){ // should PRICE_NOM null be skipped?
-                continue;
+let populationdata = [];
+function price_nom(){
+    fetch("https://services.arcgis.com/ZOyb2t4B0UYuYNYH/arcgis/rest/services/Typology/FeatureServer/0/query?where=1%3D1&outFields=YEAR,PRICE_NOM&returnGeometry=false&outSR=4326&f=json")
+        .then(response => {
+            return response.json()
+        })
+        .then(data => {
+            console.log(data.features[100])
+            console.log(data.features[255].attributes.YEAR)
+            console.log(data.features[255].attributes.PRICE_NOM)
+            chartdata = [];
+            //chartdata[0] = "Single Family Home Sales Prices"
+            for(let i = 0; i < data.features.length; i++)
+            {
+                let d = data.features[i].attributes;
+                // console.log(d);
+                if(d.YEAR == null || d.PRICE_NOM == null){ // should PRICE_NOM null be skipped?
+                    continue;
+                }
+                if(chartdata[d.YEAR - 2006] != null)
+                    chartdata[d.YEAR - 2006].pricenom += d.PRICE_NOM
+                else{
+                    chartdata[d.YEAR - 2006] = {};
+                    chartdata[d.YEAR - 2006].year = d.YEAR
+                    chartdata[d.YEAR - 2006].pricenom = d.PRICE_NOM
+                }
             }
-            if(chartdata[d.YEAR - 2006] != null)
-                chartdata[d.YEAR - 2006].pricenom += d.PRICE_NOM
-            else{
-                chartdata[d.YEAR - 2006] = {};
-                chartdata[d.YEAR - 2006].year = d.YEAR
-                chartdata[d.YEAR - 2006].pricenom = d.PRICE_NOM
-            }
-        }
-        console.log(chartdata);
-        line_chart();
-    })
+            console.log(chartdata);
+            line_chart();
+        })
+}
+//price_nom();
 
 function line_chart(){
     let chart = c3.generate({
@@ -49,7 +53,8 @@ function line_chart(){
     });
 }
 
-fetch("https://gisdata.kingcounty.gov/arcgis/rest/services/OpenDataPortal/census__demographic_base_area_esj/MapServer/25491201/query?outFields=*&where=1%3D1&f=geojson")
+function median_income_tract(){
+    fetch("https://gisdata.kingcounty.gov/arcgis/rest/services/OpenDataPortal/census__demographic_base_area_esj/MapServer/25491201/query?outFields=*&where=1%3D1&f=geojson")
     .then(response => {
         return response.json()
     })
@@ -73,6 +78,7 @@ fetch("https://gisdata.kingcounty.gov/arcgis/rest/services/OpenDataPortal/census
         console.log(chartdata);
         scatter_chart();
     }) 
+}
 
 function scatter_chart(){
     let chart = c3.generate({
@@ -101,6 +107,7 @@ function scatter_chart(){
     });
 }
 
+function demographics(){
 // NOTE: Did not include "POC" in our data calculation
 fetch("https://gisdata.kingcounty.gov/arcgis/rest/services/OpenDataPortal/census___base/MapServer/2549/query?outFields=*&where=1%3D1&f=geojson")
     .then(response => {
@@ -145,6 +152,7 @@ fetch("https://gisdata.kingcounty.gov/arcgis/rest/services/OpenDataPortal/census
         console.log(chartdata);
         bar_chart();
     })
+}
 
     function bar_chart(){
         let chart = c3.generate({
@@ -186,3 +194,105 @@ fetch("https://gisdata.kingcounty.gov/arcgis/rest/services/OpenDataPortal/census
             }
         });
     }
+
+
+function median_rent_with_pop(){
+
+    if(populationdata.length == 0){
+        fetch("https://gisdata.kingcounty.gov/arcgis/rest/services/OpenDataPortal/census__acs/MapServer/2593/query?outFields=*&where=1%3D1&f=geojson")
+        .then(response => {
+            return response.json()
+        })
+        .then(data => {
+            populationdata = data.features;
+
+            median_rent();
+        })
+    }
+}
+
+function median_rent(){
+    let chart_colors = [
+      "rgb(250, 250, 110)",
+      "rgb(156, 223, 124)",
+      "rgb(74, 189, 140)",
+      "rgb(0, 150, 142)",
+      "rgb(16, 110, 124)",
+      "rgb(160, 160, 160)"
+    ];
+    fetch("https://gisdata.kingcounty.gov/arcgis/rest/services/OpenDataPortal/census__acs/MapServer/2450/query?outFields=*&where=1%3D1&f=geojson")
+        .then(response => {
+            return response.json()
+        })
+        .then(data => {
+            console.log(data);
+            console.log(data.features[100])
+            chartdata = [];
+            chartdata[0] = new Array("$0-$1115");
+            chartdata[1] = new Array("$1115-$1309");
+            chartdata[2] = new Array("$1309-$1476");
+            chartdata[3] = new Array("$1476-$1699");
+            chartdata[4] = new Array("$1699-$3309");
+            chartdata[5] = new Array("Missing Data");
+            //chartdata[0] = "Single Family Home Sales Prices"
+
+            //console.log(populationdata[100].properties);
+            for(let i = 0; i < data.features.length; i++)
+            {
+                let d = data.features[i].properties;
+                let rent = d.E25058232;
+                let num = 1;
+                //console.log(populationdata[i].properties.GEO_ID_TRT + "," + d.GEO_ID_TRT);
+                if(populationdata.length > 0){
+                    num = populationdata[i].properties.E01003097;
+                }
+
+                console.log()
+
+                if(rent == null || rent.length > 0){
+                    chartdata[5].push(num);
+                }
+                else if(rent >= 0 && rent < 1115){
+                    chartdata[0].push(num);
+                }
+                else if(rent >= 1115 && rent < 1309){
+                    chartdata[1].push(num);
+                }
+                else if(rent >= 1309 && rent < 1476){
+                    chartdata[2].push(num);
+                }
+                else if(rent >= 1476 && rent < 1699){
+                    chartdata[3].push(num);
+                }
+                else if(rent >= 1699 && rent < 3309){
+                    chartdata[4].push(num);
+                }
+            }
+            console.log(chartdata);
+            pie_chart();
+        })
+}
+
+function pie_chart(){
+    var chart = c3.generate({
+        bindto: '#chart_median_rent',
+        data: {
+            // iris data from R
+            columns: [
+                chartdata[0],chartdata[1],chartdata[2],chartdata[3],chartdata[4], chartdata[5]
+            ],
+            legend:{
+                show:false
+            },
+            type : 'pie',
+            colors : {
+                "$0-$1115":"#FAFA6E",
+                "$1115-$1309":"#9CDF7C",
+                "$1309-$1476":"#4ABD8C",
+                "$1476-$1699":"#00968E",
+                "$1699-$3309":"#106E7C",
+                "Missing Data":"#A0A0A0"
+            }
+        }
+    });
+}
